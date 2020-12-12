a5acfe4567f707887fe55c43934df00f6d827df6
Siddharth Kundu <sk31395@gmail.com>
2020-12-12 14:54:02 +0530

    styling changes

--- pages/blog.js
+++ pages/blog.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Articles</title>
       </Head>
       <Layout>
-        <div className="w-full py-5 max-w-screen-xl mx-auto flex-1">
+        <div className="w-screen max-w-screen-md mx-auto pt-14 flex flex-col flex-auto">
           <div className="flex-1">blog page</div>
         </div>
       </Layout>
