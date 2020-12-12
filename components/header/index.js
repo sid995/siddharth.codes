1084c86bbe03a86b69ff177c2949a44a71b0de17
Siddharth Kundu <sk31395@gmail.com>
2020-12-12 14:53:30 +0530

    styling changes

--- components/header/index.js
+++ components/header/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <nav className="fixed bg-white top-0 w-screen border-color-50 border-b-2">
+    <nav className="fixed bg-white top-0 left-0 z-10 w-full border-color-50 border-b-2">
       <div className="w-full max-w-screen-md h-14 mx-auto px-4">
         <div className="flex justify-between items-center h-full">
           <Link href="/">
--- components/layout/index.js
+++ components/layout/index.js
@@ -2,7 +2,7 @@ import Footer from '@components/footer'
 import Header from '@components/header'
 
 export const Layout = props => (
-  <div className="relative flex flex-col pt-14 w-screen min-h-screen">
+  <div className="relative flex flex-col w-screen min-h-screen">
     <Header />
     {props.children}
     <Footer />
